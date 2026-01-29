7989b29e3ba7c2afa27eb7a49421d1185cc220fb
HabibMunisha <skh793933@gmail.com>
2026-01-29 21:41:07 +0530

    Add files via upload

---
+++ 10-06-2025/Ascii.py
@@ -0,0 +1,7 @@
+#write a p prog to read a char as input and print asci value of the charecter
+char = input("enter a letter: ")
+print(ord(char))
+
+#for number to ascii
+num = int(input("enter the num: "))
+print(chr(num)) 
