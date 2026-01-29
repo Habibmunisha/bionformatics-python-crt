454469109b5241bf00d0508bf314269032b03234
HabibMunisha <skh793933@gmail.com>
2026-01-29 15:52:44 +0530

    Add files via upload

---
+++ 03-06-2025/code14.py
@@ -0,0 +1,11 @@
+
+#write a py prog to read an integer value and find number of 0's present in user entered number
+num = int(input())
+count = 0
+digits = 0
+while(num!=0):
+    digits = num%10
+    if(digits == 0):
+        count = count+1
+    num = num//10
+print(count)
---
+++ 03-06-2025/code5.py
@@ -0,0 +1,5 @@
+
+#write a py program to read an integer value as input from the user and print the multiplication table of it
+num1 = int(input())
+for i in range(1,11):
+    print(f"{num1} * {i} = {i*num1}")
