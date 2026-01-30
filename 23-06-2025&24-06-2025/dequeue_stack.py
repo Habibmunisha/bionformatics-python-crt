8193e38f67b5b3ba9417ef6efa221c566f335c4f
HabibMunisha <skh793933@gmail.com>
2026-01-30 15:15:54 +0530

    Add files via upload

---
+++ 23-06-2025&24-06-2025/dequeue_stack.py
@@ -0,0 +1,12 @@
+
+from collections import deque
+stack = deque()
+stack.append('A')
+stack.append('B')
+stack.append('C')
+stack.append('D')
+
+print(f"Stack after pushing {stack}")
+top = stack.pop()
+print("popped element: ",top)
+print("stack after ")
